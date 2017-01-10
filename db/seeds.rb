0788da221e899aad540aef066c7a2ec29811e491
Sau Wai Chan <sauwaic@gmail.com>
2017-01-10 22:44:06 +0800

    With seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+CoffeeShop.destroy_all
+coffee_shops_attributes = [
+  {
+    name:         "Lof10",
+    address:      "Soho, Hong Kong",
+    wifi:         true,
+    rating:        5
+  },
+  {
+    name:         "Why50",
+    address:      "Sheung Wan, Hong Kong",
+    wifi:         true,
+    rating:        5
+  },
+  {
+    name:         "Starbucks",
+    address:      "Sheung Wan",
+    wifi:         true,
+    rating:        3
+  }
+]
+coffee_shops_attributes.each { |params| CoffeeShop.create!(params) }
